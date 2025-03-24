709ced49be926a38443d9632803098d715b35b96
Kacper Furmanczyk <k.furmanczykq@gmail.com>
2025-03-24 17:46:57 +0100

    Add project files.

---
+++ Entities/Role.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO.Packaging;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Magazyn.Entities
+{
+    internal class Role
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+    }
+}
---
+++ Entities/User.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Magazyn.Entities
+{
+    internal class User
+    {
+        public int Id { get; set; }
+        public string Login { get; set; }
+        public string Password { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string PESEL { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public string Gender{ get; set; }
+        public string Email { get; set; }
+        public string PhoneNumber { get; set; }
+        public int RoleId { get; set; }
+        public Role Role { get; set; }
+        public string AddressId { get; set; }
+        public Address Address { get; set; }
+    }
+}
