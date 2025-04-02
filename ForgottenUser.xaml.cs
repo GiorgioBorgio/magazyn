c4ebeb5b23fe8d49cc19966fc0b93b0fdf80bc84
GiorgioBorgio <m.pietrzak-gacek@proton.me>
2025-04-02 16:12:12 +0200

    Lista zapomnianych wyszukiwanie

--- ForgottenUser.xaml.cs
+++ ForgottenUser.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using Magazyn.Entities;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -18,9 +19,10 @@ namespace Magazyn
     /// <summary>
     /// Logika interakcji dla klasy DeletedUsers.xaml
     /// </summary>
-    public partial class ForgottenUser : Window
+    public partial class ForgottenUser : Window 
     {
         private WarehouseDbContext _context;
+        private List<User> _users;
         public ForgottenUser()
         {
             InitializeComponent();
@@ -28,9 +30,28 @@ namespace Magazyn
             ForgottenDataGrid.ItemsSource = _context.Users.Where(e=>e.IsForgotten == true).ToList();
         }
 
+        public void RefreshUserDataGrid(string searchText = "")
+        {
+            _context.ChangeTracker.Clear();
+            var keywords = searchText.Split(' ');
+            ForgottenDataGrid.ItemsSource = null; // Resetujemy źródło
+            var filteredUsers = _context.Users
+                .Where(user =>
+            keywords.All(kw =>
+            user.FirstName.ToLower().Contains(kw) ||
+            user.LastName.ToLower().Contains(kw) ||
+            user.Login.ToLower().Contains(kw) ||
+            user.PESEL.Contains(kw))).AsNoTracking().ToList();
+            _users = new List<User>(filteredUsers);
+            ForgottenDataGrid.ItemsSource = _users.Where(e => e.IsForgotten == true);
+        }
+
         private void TextBoxSearch_TextChanged(object sender, TextChangedEventArgs e)
         {
-
+            RefreshUserDataGrid(TextBoxSearch.Text);
+            PlaceholderText.Visibility = string.IsNullOrEmpty(TextBoxSearch.Text)
+            ? Visibility.Visible
+            : Visibility.Collapsed;
         }
     }
 }
