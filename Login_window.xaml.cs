ca99073a67651cea61e1d51467955d32ec80a953
Łukasz <ukasz@DESKTOP-5M90O28>
2025-05-27 18:13:04 +0200

    logowanie

--- Login_window.xaml.cs
+++ Login_window.xaml.cs
@@ -23,5 +23,34 @@ namespace Magazyn
         {
             InitializeComponent();
         }
+
+        private void Button_logowanie_ok_Click(object sender, RoutedEventArgs e)
+        {
+            string login = LoginTextBox.Text;
+            string password = PasswordBox.Password; // Jeśli TextBox, użyj .Text
+
+            using (var context = new Entities.WarehouseDbContext())
+            {
+                var user = context.Users
+                    .FirstOrDefault(u => u.Login == login && u.Password == password);
+
+                if (user != null)
+                {
+                    MessageBox.Show("Zalogowano pomyślnie!", "Sukces", MessageBoxButton.OK, MessageBoxImage.Information);
+
+
+                    // Otwórz nowe okno
+                    MainWindow mainWindow = new MainWindow();
+                    mainWindow.Show();
+
+                    // Zamknij to okno logowania
+                    this.Close();
+                }
+                else
+                {
+                    MessageBox.Show("Błędny login lub hasło.", "Błąd", MessageBoxButton.OK, MessageBoxImage.Error);
+                }
+            }
+        }
     }
 }
